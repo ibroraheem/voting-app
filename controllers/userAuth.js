e705245c05b2f79c15eefa707a96045cc4f334a5
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2023-08-04 07:09:10 +0100

    SSS

--- controllers/userAuth.js
+++ controllers/userAuth.js
@@ -91,7 +91,7 @@ const login = async (req, res) => {
 const verify = async (req, res) => {
     try {
         const { otp } = req.body
-        const token = req.headers.authorization.split('')[1]
+        const token = req.headers.authorization.split(' ')[1]
         if (!token) return res.status(400).json({ message: 'No token provided' })
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
         const user = await User.findOne({ _id: decoded.id })
