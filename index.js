acded1deb06a212f2c4b1fa33c60f7172687ce55
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2023-08-18 09:30:23 +0100

    Swww

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const swaggerUi = require('swagger-ui-express');
-const swaggerFile = require('./swagger_output.json');
 const connectDB = require('./config/db');
 bodyParser.urlencoded({ extended: false });
 app.use(bodyParser.json());
@@ -21,7 +19,6 @@ app.get('/', (req, res) => {
 
 app.use('/user', require('./routes/user'))
 app.use('/admin', require('./routes/admin'))
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 connectDB();
 app.listen(3000, () => {
