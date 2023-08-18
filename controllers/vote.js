f0c6336c634ee65ba0e9be2308011041c500edef
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2023-08-18 10:43:48 +0100

    Swww

--- controllers/vote.js
+++ controllers/vote.js
@@ -77,7 +77,6 @@ const getCandidates = async (req, res) => {
         if (user.hasVoted) return res.status(400).json({ message: 'User has already voted' })
         const candidates = await Candidate.find({ post: { $ne: 'SRC' } })
         let src = await Candidate.find({ post: 'SRC' })
-        src = src.filter((candidate) => candidate.department === user.department)
         res.status(200).json({ candidates, src })
     } catch (error) {
         console.log(error)
